95fb79fb84dfc9a408e1eb987f241a843dd0cd31
Bryan Sparks <bryan@drycanyon.com>
2026-06-24 12:18:44 -0600

    feat(evolve): add types + v1 routing table data

---
+++ src/armature_cabinet/evolve/__init__.py
@@ -0,0 +1,4 @@
+"""Cabinet evolve: self-improvement of richly-defined agents from run traces."""
+from .types import AgentTraceSummary, RoutingDecision, FileProposal, AgentVersion
+
+__all__ = ["AgentTraceSummary", "RoutingDecision", "FileProposal", "AgentVersion"]
